4382829d3e2013e998388f195667215092a0079b
Lennart <lennart.schmidt-98@web.de>
2019-09-28 15:47:52 +0200

    test3

--- humiditree/src/App.vue
+++ humiditree/src/App.vue
@@ -16,13 +16,15 @@
 <style>
   @import '~bootstrap/dist/css/bootstrap.css';
   @import '~bootstrap-vue/dist/bootstrap-vue.css';
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300&display=swap');
 
-  body {
-    margin: 0;
+
+ html, body {
+    font-family: 'Roboto', sans-serif;
   }
 
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: 'Roboto', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
